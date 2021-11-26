6ffff32202fd8fe286cd8d18afc85fa7d4e2d45d
MaxwellSeybert <89546325+MaxwellSeybert@users.noreply.github.com>
2021-11-26 14:00:39 -0700

    CSVtoArrayList and createTrainList
    methods to read in a csv file and convert the file to a list of train objects

--- CoalSimulator.java
+++ CoalSimulator.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CoalSimulator {
 	/*
@@ -7,22 +11,22 @@ public class CoalSimulator {
 	 * trains are in the system, and is one of them the high-capacity train? If the
 	 * high-capacity train arrives: Go to Step 7.
 	 *
-	 * • Step 2: If three standard trains are in the system, at least two are going
+	 * â€¢ Step 2: If three standard trains are in the system, at least two are going
 	 * to have to wait on hold and incur demurrage costs. Add $30,000 to cost for
-	 * this hour’s waiting. Go to Step 5.
+	 * this hourâ€™s waiting. Go to Step 5.
 	 * 
 	 * Step 3: If two trains are in the system, at least one will have to wait and
-	 * incur demurrage costs. Add $15,000 to costs for this hour’s waiting. Go to
+	 * incur demurrage costs. Add $15,000 to costs for this hourâ€™s waiting. Go to
 	 * Step 5.
 	 * 
-	 * • Step 4: If no trains are currently in the system, then let the system be
+	 * â€¢ Step 4: If no trains are currently in the system, then let the system be
 	 * idle, except check to see if the tipple is not at full capacity. If it is not
 	 * at full capacity, then begin filling it. In 1 hr, two crews can fill one-half
 	 * trainload into the tipple. Add $11,000 to cost for working crews if they fill
 	 * the tipple during this hour. Go back and check for next hour to see if trains
 	 * arrive. Proceed to next hour. Go to Step 1.
 	 * 
-	 * • Step 5: By now, we must see if we can actually fill the train that is
+	 * â€¢ Step 5: By now, we must see if we can actually fill the train that is
 	 * on-line to be filled. If so, then take one-third of a trainload away from the
 	 * tipple and put it into the train. If the train is full, send it on its way
 	 * and reset to 0 the load value of the current train being loaded. Otherwise,
@@ -30,34 +34,34 @@ public class CoalSimulator {
 	 * to the next hour. Go to Step 1.
 	 * 
 	 * 
-	 * • Step 6: If the tipple needs filling, we should fill it with two crews, add
+	 * â€¢ Step 6: If the tipple needs filling, we should fill it with two crews, add
 	 * a half trainload to the tipple, add $1,000 demurrage cost for this train to
 	 * sit on the tracks waiting for the tipple to load, and add $21,000 to the
 	 * labor costs for the crews at work. Proceed to next hour. Go to Step 1.
 	 * 
 	 * 
-	 * • Step 7: Now that a high-capacity train with five engines has arrived, to
+	 * â€¢ Step 7: Now that a high-capacity train with five engines has arrived, to
 	 * prevent exorbitant demurrage costs for making this train wait in lieu of
 	 * another, we will automatically switch to service the high-capacity train. If
 	 * another train is currently being serviced, then we will put it on hold for
 	 * now.
 	 * 
 	 * 
-	 * • Step 8: If three standard trains are now waiting, all three are going to
+	 * â€¢ Step 8: If three standard trains are now waiting, all three are going to
 	 * have to wait for the high-capacity train and incur demurrage costs. Add
-	 * $45,000 to costs for this hour’s waiting.
+	 * $45,000 to costs for this hourâ€™s waiting.
 	 * 
-	 * • Step 9: If two standard trains are now waiting for the high-capacity train
+	 * â€¢ Step 9: If two standard trains are now waiting for the high-capacity train
 	 * to be serviced, both will have to wait a qnd incur demurrage costs. Add
-	 * $30,000 to costs for this hour’s waiting.
+	 * $30,000 to costs for this hourâ€™s waiting.
 	 * 
 	 * 
-	 * • Step 10: If one standard train is now waiting for the high-capacity train
+	 * â€¢ Step 10: If one standard train is now waiting for the high-capacity train
 	 * to be serviced, then the standard train will have to incur demurrage costs
 	 * for this hour. Add $15,000 to costs for this hour of waiting.
 	 * 
 	 * 
-	 * • Step 11: By now, we must see if we can actually fill the high-capacity
+	 * â€¢ Step 11: By now, we must see if we can actually fill the high-capacity
 	 * train that is on-line to be filled. If so, then no demurrage costs will be
 	 * incurred for this one train for this hour. If the tipple needs filling, we
 	 * should fill it with two crews, add a half trainload to the tipple, add to
@@ -66,7 +70,7 @@ public class CoalSimulator {
 	 * now if the tipple was filled and the high-capacity train had to wait.
 	 * 
 	 * 
-	 * • Step 12: If we did not need to fill the tipple, then take one-third of a
+	 * â€¢ Step 12: If we did not need to fill the tipple, then take one-third of a
 	 * trainload away from the tipple and put it into the train. If the highcapacity
 	 * train is full, send it on its way and let the load value of the current train
 	 * being worked on go to 0 since it has now left. Otherwise, keep track of the
@@ -74,6 +78,72 @@ public class CoalSimulator {
 	 * and go back to Step 1.
 	 * 
 	 */
+
+	public static ArrayList<String> CSVtoArrayList(String CSVFileName) {
+		// the method that we used in homework 4 is being used again to read in the info
+		// except for this time it is information about trains
+		ArrayList<String> arrlist = new ArrayList<String>();
+
+		if (CSVFileName != null) {
+			String[] splitData = CSVFileName.split("\\,", -1); // the -1 helps handle the null values
+
+			for (int i = 0; i < splitData.length; i++) {
+				// if it is null, replace it with a 0
+				if (splitData[i].length() == 0) {
+					splitData[i] = "0";
+				}
+				// as long as it is not null and the length is not 0, trim the value and add it
+				// to the arraylist
+				if (!(splitData[i] == null) || !(splitData[i].length() == 0)) {
+					arrlist.add(splitData[i].trim());
+				}
+			}
+		}
+		return arrlist;
+	}
+
+	public static ArrayList<Train> createTrainList() {
+		ArrayList<Train> trainList = new ArrayList<Train>();
+		ArrayList<String> data = new ArrayList<String>();
+		BufferedReader readBuffer = null;
+		try {
+			// creates a string for the input line then asks the user for the file location
+			String inputLine;
+			String fileLoc = null;
+			Scanner scnr = new Scanner(System.in);
+			System.out.println("Welcome to train cost optimizer!");
+			System.out.println("To run this program a CSV file will be needed with the following format");
+			// TO-DO make this be more informative on what info is needed
+			System.out.println(
+					"Column 1 - Train ID (integer)   Column 2 - Number of Engines(integer)  Column 3 - isFull(boolean) Column 4 - fillLevel (double between 0 -1.5) Column 4 - Arrival Time (number between 1 - 24)");
+			System.out.println("Enter the file location of your train information:");
+			// takes in teh file location
+			fileLoc = scnr.nextLine();
+			readBuffer = new BufferedReader(new FileReader(fileLoc));
+			int lineCount = 0;
+			while ((inputLine = readBuffer.readLine()) != null) {
+				// reads in the file line by line and creates a train object from each line
+
+				if (lineCount != 0) {
+					data = CSVtoArrayList(inputLine);
+					int trainId = Integer.valueOf(data.get(0));
+					int numberOfEngines = Integer.valueOf(data.get(1));
+					boolean isFull = Boolean.valueOf(data.get(2));
+					double fillLevel = Double.valueOf(data.get(3));
+					double arriveTime = Double.valueOf(data.get(4));
+					trainList.add(new Train(trainId, numberOfEngines, isFull, fillLevel, arriveTime));
+				}
+				lineCount++;
+
+			}
+
+		} catch (Exception e) {
+			System.out.println("There was an error reading the file.");
+
+		}
+		// returns the list of trains
+		return (trainList);
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 		// built in for randomizing arrive and depart attribute
@@ -88,7 +158,7 @@ public class CoalSimulator {
 		// array list of trains type Train
 		ArrayList<Train> trainList = new ArrayList<Train>();
 		while (true) {
-			
+
 			if (hours == 0) {
 				trainList = new ArrayList<Train>();
 				// semi random instances of trains
@@ -119,15 +189,14 @@ public class CoalSimulator {
 				if (train.getArrivalTime() == hours) {
 					arrivedTrain = train;
 				}
-				//when a train arrives and the tipple is empty
+				// when a train arrives and the tipple is empty
 				if (train.getArrivalTime() == hours && tipple.getCurrentTrain() == null) {
-					
+
 					// prioritize high capacity
 					if (train.getNumberOfEngines() == 5) {
 						tipple.setCurrentTrain(train);
 						train.setDepartureTime(hours + 6.0);
-					}
-					else   {
+					} else {
 						tipple.setCurrentTrain(train);
 						train.setDepartureTime(hours + 3.0);
 					}
@@ -142,13 +211,13 @@ public class CoalSimulator {
 			// step 4 check tipple at zero and rounds to eliminate the decimals
 
 			if (Math.round(tipple.getTippleFillLevel() * 100) / 100 <= 0 || tipple.getCurrentTrain() == null) {
-				//crew fees done from load tipple
+				// crew fees done from load tipple
 				int temp = tipple.loadTipple();
-				if(temp != -1) {
+				if (temp != -1) {
 					crewFees = temp;
 				}
-				
-				//check for presence of train and the status of fill level 
+
+				// check for presence of train and the status of fill level
 			} else if (tipple.getCurrentTrain() != null && !tipple.getCurrentTrain().getIsFull()) {
 				demurrageFees = tipple.fillTrain(hours);
 
